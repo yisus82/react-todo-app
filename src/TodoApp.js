b97a4e57ccca7788d1bf25cc7e1cf5534a0974eb
Yago Pérez Vázquez <yagoperez@outlook.com>
2019-03-28 12:02:27 +0100

    initial-commit

---
+++ src/TodoApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoApp extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hola mundo</p>
+      </div>
+    );
+  }
+}
+
+export default TodoApp;
